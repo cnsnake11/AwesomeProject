b05e7f60ef3c79e6e3a0ba865c9ee7dc7fefa35f
cnsnake11 <cnsnake11@hotmail.com>
2015-11-25 15:59:29 +0800

    调整dev=false，提升性能

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
 
                 .addPackage(new MainReactPackage())
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
+//                .setUseDeveloperSupport(true)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
 

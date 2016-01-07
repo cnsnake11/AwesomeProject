e3e59f9441dab1bbd7bc353192cc817096e4e3d3
cnsnake11 <cnsnake11@hotmail.com>
2016-01-07 17:27:30 +0800

    重构rnm目录名

--- bbt-react-native/index.js
+++ bbt-react-native/index.js
@@ -6,26 +6,26 @@
  * 统一的对外发布出口
  * 业务使用只require这一个类即可
  */
-let BbtReactNative={
+let BbtReactNative = {
 
-    //base
-    baseCss:require('./base/BaseCss/Base.css'),
-    BaseLogicObj:require('./base/BaseLogicObj/BaseLogicObj'),
+    // base
+    baseCss: require('./base/BaseCss/Base.css'),
+    BaseLogicObj: require('./base/BaseLogicObj/BaseLogicObj'),
 
 
-    //views
-    Loading:require('./views/Loading/Loading'),
-    ListViewBindUrl:require('./views/ListViewBindUrl/ListViewBindUrl'),
-    Mask:require('./views/Mask/Mask'),
-    Modal:require('./views/Modal/Modal'),
+    // views
+    Loading: require('./views/Loading/Loading'),
+    ListViewBindUrl: require('./views/ListViewBindUrl/ListViewBindUrl'),
+    Mask: require('./views/Mask/Mask'),
+    Modal: require('./views/Modal/Modal'),
 
-    //apis
-    TabApi:require('./api/TabApi/TabApi'),
+    // apis
+    TabApi: require('./api/TabApi/TabApi'),
 
-    //RNM:react native modules
-    selectImageRNM:require('react-native').NativeModules.SelectImageRNM,
+    // RNM:react native modules
+    selectImageRNM: require('react-native').NativeModules.SelectImageRNM,
 
 };
 
 
-module.exports=BbtReactNative;
+module.exports = BbtReactNative;
